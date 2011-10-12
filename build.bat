c19d6cba02345f259a547c2c68ed0874114c5633
kpayne <kpayne@80a49c6b-0dd2-4e8f-b55d-1bcceda431ef>
2011-10-12 20:08:14 +0000

    Putting plugin in subdirectory before zipping

--- build.bat
+++ build.bat
@@ -1,2 +1,13 @@
+'-- Remove the existing zip
 del wp-profiler.zip
-"c:\Program Files (x86)\7-Zip\7z.exe" a -r -tzip -y -xr!?svn\* -x!.svn wp-profiler.zip -x!build.bat *
+
+'-- Make a temp copy
+cd ..
+rmdir /S /Q .\wp-profiler
+xcopy /S .\profiler-plugin .\wp-profiler\
+
+'-- Zip it
+"c:\Program Files (x86)\7-Zip\7z.exe" a -r -tzip -y -xr!?svn\* -x!.svn .\profiler-plugin\wp-profiler.zip -x!build.bat wp-profiler
+
+'-- Clean up
+rmdir /S /Q .\wp-profiler
