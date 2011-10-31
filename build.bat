cb66914673f68dddaa31c0fbd579cbe4ad4139b6
kpayne <kpayne@80a49c6b-0dd2-4e8f-b55d-1bcceda431ef>
2011-10-31 20:05:56 +0000

    Rebranding as starfield / P3

--- build.bat
+++ build.bat
@@ -1,13 +1,13 @@
 '-- Remove the existing zip
-del wp-profiler.zip
+del p3-profiler.zip
 
 '-- Make a temp copy
 cd ..
-rmdir /S /Q .\wp-profiler
-xcopy /S .\profiler-plugin .\wp-profiler\
+rmdir /S /Q .\p3-profiler
+xcopy /S .\profiler-plugin .\p3-profiler\
 
 '-- Zip it
-"c:\Program Files (x86)\7-Zip\7z.exe" a -r -tzip -y -xr!?svn\* -x!.svn .\profiler-plugin\wp-profiler.zip -x!build.bat wp-profiler
+"c:\Program Files (x86)\7-Zip\7z.exe" a -r -tzip -y -xr!?svn\* -x!.svn .\profiler-plugin\p3-profiler.zip -x!build.bat p3-profiler
 
 '-- Clean up
-rmdir /S /Q .\wp-profiler
+rmdir /S /Q .\p3-profiler
