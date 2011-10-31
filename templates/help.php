54f4a982c1a120a40bce7f3776f4122150631d78
kpayne <kpayne@80a49c6b-0dd2-4e8f-b55d-1bcceda431ef>
2011-10-31 22:31:03 +0000

    More help docs

--- templates/help.php
+++ templates/help.php
@@ -35,10 +35,13 @@
 		// Automatically create the table of contents
 		var links = [];
 		var i = 1;
-		$("h2.p3-help-question").each(function() {
+		$("h2.p3-help-question:not(:first)").each(function() {
 			$(this).before('<a name="q' + i + '">&nbsp;</a>');
 			links.push('<li><a href="#q' + i + '">' + $(this).html() + '</a></li>');
 			i++;
+		});
+		$("div.p3-question blockquote:not(:first)").each(function() {
+			$(this).after('<a href="#top">Back to top</a>');
 		});
 		$("#p3-help-toc").html("<ul>" + links.join("\n") + "</ul>");
 		
@@ -47,8 +50,11 @@
 </script>
 
 <div class="p3-question">
-	<h2>Contents</h2>
-	<div id="p3-help-toc"></div>
+	<a name="top">&nbsp;</a>
+	<h2 class="p3-help-question">Contents</h2>
+	<blockquote>
+		<div id="p3-help-toc"></div>
+	</blockquote>
 </div>
 
 <div class="p3-question">
@@ -69,7 +75,9 @@
 		loading.  For example, loading large images, large videos, or lots of content
 		can lead your page to render slowly.  Tools like <a href="http://www.webpagetest.org/" target="_blank">webpagetest.org</a>,
 		<a href="http://getfirebug.com/" target="_blank">Firebug</a>, <a href="http://tools.pingdom.com/" target="_blank">Pingdom tools</a>,
-		or Safari or Chrome's developer console can show you a connection breakdown of your site's content.
+		or <a href="http://developer.apple.com/technologies/safari/developer-tools.html" target="_blank">Safari Developer Tools</a>
+		or <a href="http://code.google.com/chrome/devtools/docs/overview.html" target="_blank">Chrome Developer Tools</a> can
+		show you a connection breakdown of your site's content.
 	</blockquote>
 </div>
 
@@ -221,6 +229,13 @@
 		<br /><br />
 		By default, this plugin attempts to clear any opcode caches before it runs.
 		You can change this setting in the "Advanced Settings" link under "Start Scan."
+		<br /><br />
+		Caching plugins that have an option to disable caches for logged in users will not
+		give you the same performance profile that an anonymous user would experience.  To
+		get around this, you should select a manual scan, then run an incognito browser
+		window, or run another browser, and browse your site as a logged out user.  When you're
+		finished, click "I'm done." and you're scan should show the performance of an
+		anonymous user.
 	</blockquote>
 </div>
 
@@ -246,128 +261,189 @@
 </div>
 
 <div class="p3-question">
-	<h2 class="p3-help-question">Glossary</h2>
-	<div>
-		<div id="p3-glossary-container">
-			<div class="ui-widget-header" id="p3-glossary-header" style="padding: 8px;">
-				<strong>Glossary</strong>
-				<div style="position: relative; top: 0px; right: 80px; float: right;">
-					<a href="javascript:;" id="p3-hide-glossary">Hide</a>
+	<h2 class="p3-help-question">How are these results different from YSlow / PageSpeed / Webpagetest.org / Pingdom Tools?</h2>
+	<blockquote>
+		This plugin measures how your site was generated on the server.  Tools like
+		YSlow!, PageSpeed, Webpagetest.org, and Pingdom Tools measure how your site looks
+		to the browser.
+	</blockquote>
+</div>
+
+<div class="p3-question">
+	<h2 class="p3-help-question">Is this plugin always running?</h2>
+	<blockquote>
+		The short answer is no.
+		<br /><br />
+		The more detailed answer is that the loader is always running, but checks very early on to see
+		if profiling mode is enabled, and if the user's IP address matches the configured IP address to
+		profile from.  For multisite installations, the site URL is also matched.  If these things match,
+		then profiling occurs.  Otherwise, your site loads as normal with no other code overhead.
+		<br /><br />
+		Deactivating the plugin will ensure it's not running at all without deleting your scans.  Uninstalling
+		the plugin will delete your scans.
+	</blockquote>
+</div>
+
+<div class="p3-question">
+	<h2 class="p3-help-question">How can I test specific pages on my site?</h2>
+	<blockquote>
+		When you start a scan, choose "Manual Scan" and then you can visit specific links on your site that
+		you're interested in.  If you want to profile the admin section, just click the "X" in the top right
+		of the scan window and you'll be returned to your admin section.  You can browse as normal, then come
+		back to the profile page and click "Stop Scan" when you're ready to view the results.
+		<br /><br />
+		To scan your site as an anonymous user, select "Manual Mode" as above, but instead of clicking your
+		site in the scan window, then open a different browser (or an icognito window) and browse your site
+		as a logged out user.  When you're done, close that browser and return to your admin.  Click "I'm done"
+		and view your scan results.
+	</blockquote>
+</div>
+
+<div class="p3-question">
+	<h2 class="p3-help-question">Why is my site is still slow?</h2>
+	<blockquote>
+		Your site can be slow for a number of reasons.  Your site could be under a lot of traffic, other
+		sites on your server could be under a lot of traffic, you could be referencing content from 
+		other sites that are slow, your internet connection could be slow, your server could be out of RAM,
+		your site could be very image heavy, your site could require a lot of HTTP requests, etc.
+		<br /><br />
+		Your next stop should be to <a href="http://tools.pingdom.com/" target="_blank">Pingdom Tools</a>,
+		<a href="http://webpagetest.org/" target="_blank">Webpage Test</a>, <a href="http://developer.yahoo.com/yslow/" target="_blank">YSlow</a>,
+		<a href="http://code.google.com/speed/page-speed/download.html" target="_blank">Google PageSpeed</a>, and
+		your browser's development tools like <a href="http://getfirebug.com/" target="_blank">Firebug</a> for Firefox,
+		<a href="http://code.google.com/chrome/devtools/docs/overview.html" target="_blank">Chrome Developer Tools</a> for Chrome, or
+		<a href="http://developer.apple.com/technologies/safari/developer-tools.html" target="_blank">Safari Developer Tools</a> for Safari.
+		<br /><br />
+		After you've tuned your site up as much as possible, if you're still not happy with its performance,
+		you should consult your site/server administrator or hosting support.
+	</blockquote>
+</div>
+
+<div class="p3-question">
+	<h2 class="p3-help-question" style="border-bottom-width: 0px !important;">Glossary</h2>
+	<blockquote>
+		<div>
+			<div id="p3-glossary-container">
+				<div class="ui-widget-header" id="p3-glossary-header" style="padding: 8px;">
+					<strong>Glossary</strong>
+					<div style="position: relative; top: 0px; right: 80px; float: right;">
+						<a href="javascript:;" id="p3-hide-glossary">Hide</a>
+					</div>
+				</div>
+				<div>
+					<table class="p3-results-table" id="p3-glossary-table" cellpadding="0" cellspacing="0" border="0">
+						<tbody>
+							<tr>
+								<td colspan="2" style="border-left-width: 1px !important;">
+									<div id="glossary">
+										<table width="100%" cellpadding="0" cellspacing="0" border="0" id="glossary-terms">
+											<tr>
+												<td width="200" class="term"><strong>Total Load Time</strong>
+													<div id="total-load-time-definition" style="display: none;" class="definition">
+														How long the site took to load.  This is an observed measurement (start timing when the page was requested,
+														stop timing when the page was delivered to the browser, calcuate the difference).  Lower is better.
+													</div>
+												</td>
+												<td width="400" rowspan="12" id="p3-glossary-term-display">&nbsp;</td>
+											</tr>
+											<tr>
+												<td class="term"><strong>Site Load Time</strong>
+													<div id="site-load-time-definition" style="display: none;" class="definition">
+														The calculated total load time minus the profile overhead.  This is closer to your site's real-life load time.  Lower is better.
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td class="term"><strong>Profile Overhead</strong>
+													<div id="profile-overhead-definition" style="display: none;" class="definition">
+														The load time spent in the profiling code.  Since using the profiler will slow down your load time, it is important
+														to know how much impact the profiler is having on your site.  This won't impact your site's real-life load time.
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td class="term"><strong>Plugin Load Time</strong>
+													<div id="plugin-load-time-definition" style="display: none;" class="definition">
+														The load time spent in plugins.  Because of the way WordPress is built, a function call can be traced from a
+														plugin through a theme through the core.  The profiler prioritizes plugin calls first, theme calls second, and
+														core calls last.  Lower is better.
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td class="term"><strong>Theme Load Time</strong>
+													<div id="theme-load-time-definition" style="display: none;" class="definition">
+														The load time spent in the theme.  Because of the way WordPress is built, a function call can be traced from a
+														plugin through a theme through the core.  The profiler prioritizes plugin calls first, theme calls second, and
+														core calls last.  Lower is better.
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td class="term"><strong>Core Load Time</strong>
+													<div id="core-load-time-definition" style="display: none;" class="definition">
+														The load time spent in the WordPress core.  Because of the way WordPress is built, a function call can be traced from a
+														plugin through a theme through the core.  The profiler prioritizes plugin calls first, theme calls second, and
+														core calls last.  This will probably be constant.
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td class="term"><strong>Margin of Error</strong>
+													<div id="drift-definition" style="display: none;" class="definition">
+														This is the difference between the observed runtime (what actually happened) and expected runtime (adding up the plugin
+														runtime, theme runtime, core runtime, and profiler overhead).  There are several reasons this margin of error can exist.
+														Most likely, the profiler is missing microsends while it's doing math to add up the runtime it just observed.  Using a
+														network clock to set the time (NTP) can also cause minute timing changes.  Ideally, this number should be zero, but
+														there's nothing you can do to change it.  It will give you an idea of how accurate the other results are.
+														</ul>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td class="term"><strong>Observed</strong>
+													<div id="observed-definition" style="display: none;" class="definition">
+														How long the site took to load.  This is an observed measurement (start timing when the page was requested,
+														stop timing when the page was delivered to the browser, calcuate the difference).
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td class="term"><strong>Expected</strong>
+													<div id="expected-definition" style="display: none;" class="definition">
+														The expected site load time calculated by adding plugin load time + core load time + theme load time + profiler overhead.
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td class="term"><strong>Plugin Function Calls</strong>
+													<div id="plugin-funciton-calls-definition" style="display: none;" class="definition">
+														The number of php function calls generated by a plugin.  Lower is better.
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td class="term"><strong>Memory Usage</strong>
+													<div id="memory-usage-definition" style="display: none;" class="definition">
+														The amount of RAM usage observed.  This is reporeted by <a href="http://php.net/memory_get_peak_usage" target="_blank">memory_get_peak_usage()</a>.  Lower is better.
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td class="term"><strong>MySQL Queries</strong>
+													<div id="mysql-queries-definition" style="display: none;" class="definition">
+														The count of queries sent to the database.  This reported by the WordPress function <a href="http://codex.wordpress.org/Function_Reference/get_num_queries" target="_new">get_num_queries()</a>.  Lower is better.
+													</div>
+												</td>
+											</tr>
+										</table>
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
 				</div>
 			</div>
-			<div>
-			<table class="p3-results-table" id="p3-glossary-table" cellpadding="0" cellspacing="0" border="0">
-				<tbody>
-					<tr>
-						<td colspan="2" style="border-left-width: 1px !important;">
-							<div id="glossary">
-								<table width="100%" cellpadding="0" cellspacing="0" border="0" id="glossary-terms">
-									<tr>
-										<td width="200" class="term"><strong>Total Load Time</strong>
-											<div id="total-load-time-definition" style="display: none;" class="definition">
-												How long the site took to load.  This is an observed measurement (start timing when the page was requested,
-												stop timing when the page was delivered to the browser, calcuate the difference).  Lower is better.
-											</div>
-										</td>
-										<td width="400" rowspan="12" id="p3-glossary-term-display">&nbsp;</td>
-									</tr>
-									<tr>
-										<td class="term"><strong>Site Load Time</strong>
-											<div id="site-load-time-definition" style="display: none;" class="definition">
-												The calculated total load time minus the profile overhead.  This is closer to your site's real-life load time.  Lower is better.
-											</div>
-										</td>
-									</tr>
-									<tr>
-										<td class="term"><strong>Profile Overhead</strong>
-											<div id="profile-overhead-definition" style="display: none;" class="definition">
-												The load time spent in the profiling code.  Since using the profiler will slow down your load time, it is important
-												to know how much impact the profiler is having on your site.  This won't impact your site's real-life load time.
-											</div>
-										</td>
-									</tr>
-									<tr>
-										<td class="term"><strong>Plugin Load Time</strong>
-											<div id="plugin-load-time-definition" style="display: none;" class="definition">
-												The load time spent in plugins.  Because of the way WordPress is built, a function call can be traced from a
-												plugin through a theme through the core.  The profiler prioritizes plugin calls first, theme calls second, and
-												core calls last.  Lower is better.
-											</div>
-										</td>
-									</tr>
-									<tr>
-										<td class="term"><strong>Theme Load Time</strong>
-											<div id="theme-load-time-definition" style="display: none;" class="definition">
-												The load time spent in the theme.  Because of the way WordPress is built, a function call can be traced from a
-												plugin through a theme through the core.  The profiler prioritizes plugin calls first, theme calls second, and
-												core calls last.  Lower is better.
-											</div>
-										</td>
-									</tr>
-									<tr>
-										<td class="term"><strong>Core Load Time</strong>
-											<div id="core-load-time-definition" style="display: none;" class="definition">
-												The load time spent in the WordPress core.  Because of the way WordPress is built, a function call can be traced from a
-												plugin through a theme through the core.  The profiler prioritizes plugin calls first, theme calls second, and
-												core calls last.  This will probably be constant.
-											</div>
-										</td>
-									</tr>
-									<tr>
-										<td class="term"><strong>Margin of Error</strong>
-											<div id="drift-definition" style="display: none;" class="definition">
-												This is the difference between the observed runtime (what actually happened) and expected runtime (adding up the plugin
-												runtime, theme runtime, core runtime, and profiler overhead).  There are several reasons this margin of error can exist.
-												Most likely, the profiler is missing microsends while it's doing math to add up the runtime it just observed.  Using a
-												network clock to set the time (NTP) can also cause minute timing changes.  Ideally, this number should be zero, but
-												there's nothing you can do to change it.  It will give you an idea of how accurate the other results are.
-												</ul>
-											</div>
-										</td>
-									</tr>
-									<tr>
-										<td class="term"><strong>Observed</strong>
-											<div id="observed-definition" style="display: none;" class="definition">
-												How long the site took to load.  This is an observed measurement (start timing when the page was requested,
-												stop timing when the page was delivered to the browser, calcuate the difference).
-											</div>
-										</td>
-									</tr>
-									<tr>
-										<td class="term"><strong>Expected</strong>
-											<div id="expected-definition" style="display: none;" class="definition">
-												The expected site load time calculated by adding plugin load time + core load time + theme load time + profiler overhead.
-											</div>
-										</td>
-									</tr>
-									<tr>
-										<td class="term"><strong>Plugin Function Calls</strong>
-											<div id="plugin-funciton-calls-definition" style="display: none;" class="definition">
-												The number of php function calls generated by a plugin.  Lower is better.
-											</div>
-										</td>
-									</tr>
-									<tr>
-										<td class="term"><strong>Memory Usage</strong>
-											<div id="memory-usage-definition" style="display: none;" class="definition">
-												The amount of RAM usage observed.  This is reporeted by <a href="http://php.net/memory_get_peak_usage" target="_blank">memory_get_peak_usage()</a>.  Lower is better.
-											</div>
-										</td>
-									</tr>
-									<tr>
-										<td class="term"><strong>MySQL Queries</strong>
-											<div id="mysql-queries-definition" style="display: none;" class="definition">
-												The count of queries sent to the database.  This reported by the WordPress function <a href="http://codex.wordpress.org/Function_Reference/get_num_queries" target="_new">get_num_queries()</a>.  Lower is better.
-											</div>
-										</td>
-									</tr>
-								</table>
-							</div>
-						</td>
-					</tr>
-				</tbody>
-			</table>
 		</div>
-	</div>
-	</div>
+	</blockquote>
 </div>
