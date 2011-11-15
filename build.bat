8ce67e9dd50dbac91a45e9be9006e8e3317e5ddd
kpayne <kpayne@80a49c6b-0dd2-4e8f-b55d-1bcceda431ef>
2011-11-15 00:40:28 +0000

    Only build /THIS/ plugin.  It was also building the welcome plugin .... 7-Zip is functionally challenged.

--- build.bat
+++ build.bat
@@ -7,7 +7,7 @@ rmdir /S /Q .\p3-profiler
 xcopy /S .\profiler-plugin .\p3-profiler\
 
 '-- Zip it
-"c:\Program Files (x86)\7-Zip\7z.exe" a -r -tzip -y -xr!?svn\* -x!.svn .\profiler-plugin\p3-profiler.zip -x!build.bat p3-profiler
+"c:\Program Files (x86)\7-Zip\7z.exe" a -r -tzip -y -xr!?svn\* -x!.svn .\profiler-plugin\p3-profiler.zip -x!build.bat p3-profiler\*
 
 '-- Clean up
 rmdir /S /Q .\p3-profiler
