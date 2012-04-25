582d06c26125ef633c81d19ed9e0884f0ae9014b
kpayne <kpayne@80a49c6b-0dd2-4e8f-b55d-1bcceda431ef>
2012-04-25 23:43:43 +0000

    More localization

--- templates/callouts.php
+++ templates/callouts.php
@@ -58,7 +58,7 @@ if ( !defined('P3_PATH') )
 			// Turn on the profiler
 			jQuery.post( ajaxurl, data, function( response ) {
 				if ( 1 != response ) {
-					alert( "There was an error processing your request.  Please reload the page and try again. [" + response + "]");
+					alert( "<?php _e( 'There was an error processing your request.  Please reload the page and try again.', 'p3-profiler' ); ?> [" + response + "]");
 				} else {
 
 					// Start scanning pages
@@ -81,7 +81,7 @@ if ( !defined('P3_PATH') )
 			}
 			jQuery.post( ajaxurl, data, function( response ) {
 				if ( response.indexOf( '.json' ) < 0 ) {
-					alert( "There was an error processing your request.  Please reload the page and try again. [" + response + "]");
+					alert( "<?php _e( 'There was an error processing your request.  Please reload the page and try again.', 'p3-profiler' ); ?> [" + response + "]");
 				}
 
 				// Hide the cancel button
@@ -96,7 +96,7 @@ if ( !defined('P3_PATH') )
 				P3_Scan.paused = true;
 				
 				// Update the caption
-				jQuery( "#p3-scanning-caption" ).html( "Scanning is paused." ).css( "color", "black" );
+				jQuery( "#p3-scanning-caption" ).html( "<?php _e( 'Scanning is paused.', 'p3-profiler' ); ?>" ).css( "color", "black" );
 			});
 		},
 
@@ -116,7 +116,7 @@ if ( !defined('P3_PATH') )
 			// Turn on the profiler
 			jQuery.post( ajaxurl, data, function( response ) {
 				if ( 1 != response ) {
-					alert( "There was an error processing your request.  Please reload the page and try again. [" + response + "]");
+					alert( "<?php _e( 'There was an error processing your request.  Please reload the page and try again.', 'p3-profiler' ); ?> [" + response + "]");
 				} else {
 
 					// Show the cancel button
@@ -140,7 +140,7 @@ if ( !defined('P3_PATH') )
 			}
 			jQuery.post( ajaxurl, data, function( response ) {
 				if ( response.indexOf( '.json' ) < 0 ) {
-					alert( "There was an error processing your request.  Please reload the page and try again. [" + response + "]");
+					alert( "<?php _e( 'There was an error processing your request.  Please reload the page and try again.', 'p3-profiler' ); ?> [" + response + "]");
 				}
 				
 				// Hide the cancel button
@@ -152,7 +152,7 @@ if ( !defined('P3_PATH') )
 				jQuery( "#p3-view-results-submit" ).attr( "data-scan-name", response );
 				
 				// Update the caption
-				jQuery( "#p3-scanning-caption" ).html( "Scanning is complete." ).css( "color", "black" );
+				jQuery( "#p3-scanning-caption" ).html( "<?php _e( 'Scanning is complete.', 'p3-profiler' ); ?>" ).css( "color", "black" );
 			});
 		},
 
@@ -232,7 +232,7 @@ if ( !defined('P3_PATH') )
 						}
 						$.post( ajaxurl, data, function( response ) {
 							if ( 1 != response ) {
-								alert( "There was an error saving your settings.  Please reload the page and try again. [" + response + "]");
+								alert( "<?php _e( 'There was an error processing your request.  Please reload the page and try again.', 'p3-profiler' ); ?> [" + response + "]");
 							}
 							$( "#p3-ip-dialog" ).dialog( "close" );
 						});
@@ -338,7 +338,7 @@ if ( !defined('P3_PATH') )
 			}
 			jQuery.post( ajaxurl, data, function( response ) {
 				if ( response.indexOf( '.json' ) < 0 ) {
-					alert( "There was an error processing your request.  Please reload the page and try again. [" + response + "]");
+					alert( "<?php _e( 'There was an error processing your request.  Please reload the page and try again.', 'p3-profiler' ); ?> [" + response + "]");
 				}
 				location.reload();
 			});
@@ -387,7 +387,7 @@ if ( !defined('P3_PATH') )
 			// Turn on the profiler
 			jQuery.post( ajaxurl, data, function( response ) {
 				if ( 1 != response ) {
-					alert( "There was an error processing your request.  Please reload the page and try again. [" + response + "]");
+					alert( "<?php _e( 'There was an error processing your request.  Please reload the page and try again.', 'p3-profiler' ); ?> [" + response + "]");
 				}
 			});
 
@@ -404,7 +404,7 @@ if ( !defined('P3_PATH') )
 			}
 			jQuery.post( ajaxurl, data, function( response ) {
 				if ( response.indexOf( '.json' ) < 0 ) {
-					alert( "There was an error processing your request.  Please reload the page and try again. [" + response + "]");
+					alert( "<?php _e( 'There was an error processing your request.  Please reload the page and try again.', 'p3-profiler' ); ?> [" + response + "]");
 				} else {
 					location.href = "<?php echo add_query_arg( array( 'p3_action' => 'view-scan', 'current_scan' => '1', 'name' => null ) ); ?>&name=" + response;
 				}
@@ -503,17 +503,17 @@ if ( !defined('P3_PATH') )
 
 					<strong>IP:</strong><?php echo htmlentities( $info['ip'] ); ?>
 					<div class="p3-big-button"><input type="checkbox" checked="checked" id="p3-stop-scan-submit" />
-					<label for="p3-stop-scan-submit">Stop Scan</label></div>
+					<label for="p3-stop-scan-submit"><?php _e( 'Stop Scan', 'p3-profiler' ); ?></label></div>
 					<?php echo htmlentities( $info['name'] ); ?>
 
 				<?php } else { ?>
 
 					<!-- Start scan button -->
 					<?php echo wp_nonce_field( 'p3_ajax_start_scan', 'p3_nonce' ); ?>
-					<strong>My IP:</strong><?php echo htmlentities( $GLOBALS['p3_profiler']->get_ip() ); ?>
+					<strong><?php _e( 'My IP:', 'p3-profiler' ); ?></strong><?php echo htmlentities( $GLOBALS['p3_profiler']->get_ip() ); ?>
 					<div class="p3-big-button"><input type="checkbox" checked="checked" id="p3-start-scan-submit" />
-					<label for="p3-start-scan-submit">Start Scan</label></div>
-					<a href="javascript:;" id="p3-advanced-settings">Advanced Settings</a>
+					<label for="p3-start-scan-submit"><?php _e( 'Start Scan', 'p3-profiler' ); ?></label></div>
+					<a href="javascript:;" id="p3-advanced-settings"><?php _e( 'Advanced Settings', 'p3-profiler' ); ?></a>
 
 				<?php } ?>
 			</div>
@@ -521,9 +521,9 @@ if ( !defined('P3_PATH') )
 
 		<!-- First callout cell -->
 		<td class="p3-callout">
-			<div class="p3-callout-outer-wrapper qtip-tip" title="Total number of active plugins, including must-use plugins, on your site.">
+			<div class="p3-callout-outer-wrapper qtip-tip" title="<?php _e( 'Total number of active plugins, including must-use plugins, on your site.', 'p3-profiler' ); ?>">
 				<div class="p3-callout-inner-wrapper">
-					<div class="p3-callout-caption">Total Plugins:</div>
+					<div class="p3-callout-caption"><?php _e( 'Total Plugins:', 'p3-profiler' ); ?></div>
 					<div class="p3-callout-data">
 						<?php
 						// Get the total number of plugins
@@ -536,61 +536,61 @@ if ( !defined('P3_PATH') )
 						echo $active_plugins;
 						?>
 					</div>
-					<div class="p3-callout-caption">(currently active)</div>
+					<div class="p3-callout-caption">(<?php _e( 'currently active', 'p3-profiler' ); ?>)</div>
 				</div>
 			</div>
 		</td>
 
 		<!-- Second callout cell -->
 		<td class="p3-callout">
-			<div class="p3-callout-outer-wrapper qtip-tip" title="Total number of seconds dedicated to plugin code per visit on your site."
-				<?php if ( !empty( $this->scan ) ) { ?>title="From <?php echo basename( $this->scan ); ?><?php } ?>">
+			<div class="p3-callout-outer-wrapper qtip-tip" title="<?php _e( 'Total number of seconds dedicated to plugin code per visit on your site.', 'p3-profiler' ); ?>">
+				<?php if ( !empty( $this->scan ) ) { ?>title="<?php _e( 'From', 'p3-profiler' ); ?> <?php echo basename( $this->scan ); ?><?php } ?>">
 				<div class="p3-callout-inner-wrapper">
-					<div class="p3-callout-caption">Plugin Load Time</div>
+					<div class="p3-callout-caption"><?php _e( 'Plugin Load Time', 'p3-profiler' ); ?></div>
 					<div class="p3-callout-data">
 						<?php if ( null === $this->profile ) { ?>
-							<span class="p3-faded-grey">n/a</span>
+							<span class="p3-faded-grey"><?php _e( 'n/a', 'p3-profiler' ); ?></span>
 						<?php } else { ?>
 							<?php printf( '%.3f', $this->profile->averages['plugins'] ); ?>
 						<?php } ?>
 					</div>
-					<div class="p3-callout-caption">(sec. per visit)</div>
+					<div class="p3-callout-caption">(<?php _e( 'sec. per visit', 'p3-profiler' ); ?>)</div>
 				</div>
 			</div>
 		</td>
 
 		<!-- Third callout cell -->
 		<td class="p3-callout">
-			<div class="p3-callout-outer-wrapper qtip-tip" title="Percent of load time on your site dedicated to plugin code."
-				<?php if ( !empty( $this->scan ) ) { ?>title="From <?php echo basename( $this->scan ); ?><?php } ?>">
+			<div class="p3-callout-outer-wrapper qtip-tip" title="<?php _e( 'Percent of load time on your site dedicated to plugin code', 'p3-profiler' ); ?>"
+				<?php if ( !empty( $this->scan ) ) { ?>title="<?php _e( 'From', 'p3-profiler' ); ?> <?php echo basename( $this->scan ); ?><?php } ?>">
 				<div class="p3-callout-inner-wrapper">
-					<div class="p3-callout-caption">Plugin Impact</div>
+					<div class="p3-callout-caption"><?php _e( 'Plugin Impact', 'p3-profiler' ); ?></div>
 					<div class="p3-callout-data">
 						<?php if ( null === $this->profile ) { ?>
-							<span class="p3-faded-grey">n/a</span>
+							<span class="p3-faded-grey"><?php _e( 'n/a', 'p3-profiler' ); ?></span>
 						<?php } else { ?>
 							<?php printf( '%.1f%%', $this->profile->averages['plugin_impact'] ); ?>
 						<?php } ?>
 					</div>
-					<div class="p3-callout-caption">(of page load time)</div>
+					<div class="p3-callout-caption">(<?php _e( 'of page load time', 'p3-profiler' ); ?>)</div>
 				</div>
 			</div>
 		</td>
 
 		<!-- Fourth callout cell -->
 		<td class="p3-callout">
-			<div class="p3-callout-outer-wrapper qtip-tip" title="Total number of database queries per visit."
-				<?php if ( !empty( $this->scan ) ) { ?>title="From <?php echo basename( $this->scan ); ?><?php } ?>">
+			<div class="p3-callout-outer-wrapper qtip-tip" title="<?php _e( 'Total number of database queries per visit', 'p3-profiler' ); ?>"
+				<?php if ( !empty( $this->scan ) ) { ?>title="<?php _e( 'From', 'p3-profiler' ); ?> <?php echo basename( $this->scan ); ?><?php } ?>">
 				<div class="p3-callout-inner-wrapper">
-					<div class="p3-callout-caption">MySQL Queries</div>
+					<div class="p3-callout-caption"><?php _e( 'MySQL Queries', 'p3-profiler' ); ?></div>
 					<div class="p3-callout-data">
 						<?php if ( null === $this->profile ) { ?>
-							<span class="p3-faded-grey">n/a</span>
+							<span class="p3-faded-grey"><?php _e( 'n/a', 'p3-profiler' ); ?></span>
 						<?php } else { ?>
 							<?php echo round( $this->profile->averages['queries'] ); ?>
 						<?php } ?>
 					</div>
-					<div class="p3-callout-caption">per visit</div>
+					<div class="p3-callout-caption"><?php _e( 'per visit', 'p3-profiler' ); ?></div>
 				</div>
 			</div>
 		</td>
@@ -601,34 +601,38 @@ if ( !defined('P3_PATH') )
 <!-- Dialog for IP settings -->
 <div id="p3-ip-dialog" class="p3-dialog">
 	<div>
-		IP address or pattern:<br /><br />
+		<?php _e( 'IP address or pattern:', 'p3-profiler' ); ?><br /><br />
 		<input type="checkbox" id="p3-use-current-ip" <?php if ( true == get_option( 'p3-profiler_use_current_ip' ) ) : ?>checked="checked"<?php endif; ?> />
-		<label for="p3-use-current-ip">Use my IP address</label>
+		<label for="p3-use-current-ip"><?php _e( 'Use my IP address', 'p3-profiler' ); ?></label>
 		<br />
-		<input type="text" id="p3-advanced-ip" style="width:90%;" size="35" value="" title="Enter IP address or regular expression pattern" />
+		<input type="text" id="p3-advanced-ip" style="width:90%;" size="35" value="" title="<?php _e( 'Enter IP address or regular expression pattern', 'p3-profiler' ); ?>" />
 		<br />
-		<em class="p3-em">Example: 1.2.3.4 or ( 1.2.3.4|4.5.6.7 )</em>
+		<em class="p3-em"><?php _e( 'Example: 1.2.3.4 or ( 1.2.3.4|4.5.6.7 )', 'p3-profiler' ); ?></em>
 	</div>
 	<br />
 	<div>
 		<input type="checkbox" id="p3-disable-opcode-cache" <?php if ( true == get_option( 'p3-profiler_disable_opcode_cache' ) ) : ?>checked="checked"<?php endif; ?> />
-		<label for="p3-disable-opcode-cache">Attempt to disable opcode optimizers <em>(recommended)</em></label>
+		<label for="p3-disable-opcode-cache"><?php _e( 'Attempt to disable opcode optimizers', 'p3-profiler' ); ?> <em>(<?php _e( 'recommended', 'p3-profiler' ); ?>)</em></label>
 		<br />
-		<em class="p3-em">This can increase accuracy in plugin detection, but decrease accuracy in timing</em>
+		<em class="p3-em"><?php _e( 'This can increase accuracy in plugin detection, but decrease accuracy in timing', 'p3-profiler' ); ?></em>
 	</div>
 	<br />
 	<div>
 		<input type="checkbox" id="p3-cache-buster" <?php if ( true == get_option( 'p3-profiler_cache_buster' ) ) : ?>checked="checked"<?php endif; ?> />
 		<label for="p3-cache-buster">Attempt to circumvent browser cache</label>
 		<br />
-		<em class="p3-em">This may help fix a "No visits recorded" error message.  See the <a href="<?php echo add_query_arg( array( 'p3_action' => 'help', 'current_scan' => null ) ); ?>#q-debug-log">help</a> page for details.</em>
+		<em class="p3-em"><?php printf( __('This may help fix a "No visits recorded" error message.  See the <a href="%s">help</a> page for details.', 'p3-profiler' ),
+			add_query_arg( array( 'p3_action' => 'help', 'current_scan' => null ) ) . '#q-debug-log'
+		); ?> </em>
 	</div>
 	<br />
 	<div>
 		<input type="checkbox" id="p3-debug" <?php if ( true == get_option( 'p3-profiler_debug' ) ) : ?>checked="checked"<?php endif; ?> />
-		<label for="p3-debug">Debug mode</label>
+		<label for="p3-debug"><?php _e( 'Debug mode', 'p3-profiler' ); ?></label>
 		<br />
-		<em class="p3-em">This will log the last 100 visits.  Check the <a href="<?php echo add_query_arg( array( 'p3_action' => 'help', 'current_scan' => null ) ); ?>#q-debug-log">help</a> page to view log messages.</em>
+		<em class="p3-em"><?php printf( __('This will log the last 100 visits.  Check the <a href="%s">help</a> page to view log messages.', 'p3-profiler' ),
+			add_query_arg( array( 'p3_action' => 'help', 'current_scan' => null ) ) . '#q-debug-log'
+		); ?></em>
 	</div>
 </div>
 
@@ -638,23 +642,20 @@ if ( !defined('P3_PATH') )
 		data-defaultsrc="<?php echo ( true === force_ssl_admin() ?  str_replace( 'http://', 'https://', home_url() ) :  home_url() ); ?>">
 	</iframe>
 	<div id="p3-scan-caption">
-		The scanner will analyze the speed and resource usage of all active plugins on your website.
-		It may take several minutes, and this window must remain open for the scan to finish successfully. 
+		<?php _e( 'The scanner will analyze the speed and resource usage of all active plugins on your website. It may take several minutes, and this window must remain open for the scan to finish successfully.', 'p3-profiler' ); ?>
 	</div>
 	<div id="p3-manual-scan-caption" style="display: none;">
 		<table>
 			<tr>
 				<td>
-					Click the links and pages of your site, and the scanner will
-					analyze the speed and resource usage of all of your active
-					plugins.
+					<?php _e( 'Click the links and pages of your site, and the scanner will analyze the speed and resource usage of all of your active plugins.', 'p3-profiler' ); ?>
 				</td>
 				<td width="220">
-					<a href="javascript:;" id="p3-manual-scan-cancel">Cancel</a>
+					<a href="javascript:;" id="p3-manual-scan-cancel"><?php _e( 'Cancel', 'p3-profiler' ); ?></a>
 					&nbsp;&nbsp;&nbsp;
 					<span class="p3-big-button">
 						<input type="checkbox" id="p3-manual-scan-done-submit" checked="checked" />
-						<label for="p3-manual-scan-done-submit">I'm Done</label>
+						<label for="p3-manual-scan-done-submit"><?php _e( "I'm Done", 'p3-profiler' ); ?></label>
 					</span>
 				</td>
 			</tr>
@@ -664,40 +665,40 @@ if ( !defined('P3_PATH') )
 
 <!-- Dialog for choose manual or auto scan  -->
 <div id="p3-scan-name-dialog" class="p3-dialog">
-	<div style="padding-top: 10px;">Scan name:
-		<input type="text" name="p3_scan_name" id="p3-scan-name" title="Enter scan name here"
+	<div style="padding-top: 10px;"><?php _e( 'Scan name:', 'p3-profiler' ); ?>
+		<input type="text" name="p3_scan_name" id="p3-scan-name" title="<?php _e( 'Enter scan name here', 'p3-profiler' ); ?>"
 			value="scan_<?php echo date( 'Y-m-d' ); ?>_<?php echo substr( md5( uniqid() ), -8 );?>" size="35" maxlength="100" />
 	</div>
-	<div style="padding-top: 10px;"><em class="p3-em">Enter the name of a previous scan to continue scanning</em></div>
+	<div style="padding-top: 10px;"><em class="p3-em"><?php _e( 'Enter the name of a previous scan to continue scanning', 'p3-profiler' ); ?></em></div>
 	<br />
 	<div class="p3-big-button">
-		<input type="checkbox" id="p3-auto-scan-submit" checked="checked" /><label for="p3-auto-scan-submit">Auto Scan</label>
-		<input type="checkbox" id="p3-manual-scan-submit" checked="checked" /><label for="p3-manual-scan-submit">Manual Scan</label>
+		<input type="checkbox" id="p3-auto-scan-submit" checked="checked" /><label for="p3-auto-scan-submit"><?php _e( 'Auto Scan' , 'p3-profiler' ); ?></label>
+		<input type="checkbox" id="p3-manual-scan-submit" checked="checked" /><label for="p3-manual-scan-submit"><?php _e( 'Manual Scan', 'p3-profiler' ); ?></label>
 	</div>
 </div>
 
 <!-- Dialog for progress bar -->
 <div id="p3-progress-dialog" class="p3-dialog">
 	<div id="p3-scanning-caption">
-		Scanning ...
+		<?php _e( 'Scanning ...', 'p3-profiler' ); ?>
 	</div>
 	<div id="p3-progress"></div>
 	
 	<!-- Cancel button -->
 	<div class="p3-big-button" id="p3-cancel-scan-buttonset">
-		<input type="checkbox" id="p3-cancel-scan-submit" checked="checked" /><label for="p3-cancel-scan-submit">Stop Scan</label>
+		<input type="checkbox" id="p3-cancel-scan-submit" checked="checked" /><label for="p3-cancel-scan-submit"><?php _e( 'Stop Scan', 'p3-profiler' ); ?></label>
 	</div>
 
 	<!-- View / resume buttons -->
 	<div class="p3-big-button" id="p3-resume-scan-buttonset" style="display: none;">
-		<input type="checkbox" id="p3-resume-scan-submit" checked="checked" /><label for="p3-resume-scan-submit">Resume</label>
+		<input type="checkbox" id="p3-resume-scan-submit" checked="checked" /><label for="p3-resume-scan-submit"><?php _e( 'Resume', 'p3-profiler' ); ?></label>
 		<input type="checkbox" id="p3-view-incomplete-results-submit" checked="checked" data-scan-name="" />
-		<label for="p3-view-incomplete-results-submit">View Results</label>
+		<label for="p3-view-incomplete-results-submit"><?php _e( 'View Results', 'p3-profiler' ); ?></label>
 	</div>
 	
 	<!-- View results button -->
 	<div class="p3-big-button" id="p3-view-results-buttonset" style="display: none;">
 		<input type="checkbox" id="p3-view-results-submit" checked="checked" data-scan-name="" />
-		<label for="p3-view-results-submit">View Results</label>
+		<label for="p3-view-results-submit"><?php _e( 'View Results', 'p3-profiler' ); ?></label>
 	</div>	
 </div>
